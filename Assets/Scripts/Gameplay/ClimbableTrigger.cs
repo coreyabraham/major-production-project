6afb10ef24ef0b6813dd07bffb561b68c39ea763
Brooke <brookefeely@gmail.com>
2024-08-27 15:15:14 +1000

    Update

--- Assets/Scripts/Gameplay/ClimbableTrigger.cs
+++ Assets/Scripts/Gameplay/ClimbableTrigger.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
-
+ 
 public class ClimbableTrigger : MonoBehaviour
 {
-
+ 
     [field: SerializeField] private GameObject PipePoint;
     [field: Tooltip("The point in world space that the player will warp to when detaching from the pipe. Can be left empty if \"Use Ground Point\" is false.")]
     [field: SerializeField] private GameObject GroundPoint;
-
+ 
     [field: Tooltip("Determines if the \"Ground Point\" variable should be used. If set as false, then \"Ground Point\" can be left empty.")]
     [field: SerializeField] private bool useGroundPoint;
-
-
+ 
+    
     private bool SkipJumpToClimbCheck = false;  // Used to disallow player to reattach to pipe they're already in the trigger of.
     private PlayerSystem playerSystem;
-
-
+ 
+ 
     private void DetermineClimbHook()
     {
         if (playerSystem.IsJumpingFromClimb && !SkipJumpToClimbCheck)
@@ -22,15 +22,15 @@ public class ClimbableTrigger : MonoBehaviour
             playerSystem.IsClimbing = true;
             playerSystem.IsJumpingFromClimb = false;
             playerSystem.SetVelocity(Vector3.zero);
-
+ 
             SkipJumpToClimbCheck = true;
         }
-
+ 
         if (!playerSystem.ClimbingRequested) return;
-
+ 
         playerSystem.IsClimbing = !playerSystem.IsClimbing;
         playerSystem.SetVelocity(Vector3.zero);
-
+ 
         if (playerSystem.IsClimbing)
         {
             playerSystem.Warp(PipePoint.transform.position);
@@ -42,15 +42,15 @@ public class ClimbableTrigger : MonoBehaviour
         }
         playerSystem.ClimbingRequested = false;
     }
-
+ 
     private void Update()
     {
         if (!playerSystem) return;
-
+ 
         DetermineClimbHook();
-
+ 
         bool isOutsideBounds = false;
-
+ 
         /*Buggy
          * - Auto - Detach from Pipe if you pass beyond it's PipePoint transform.
         //if (playerSystem.IsClimbing && toggleCooldownTimer <= 0)
@@ -77,24 +77,24 @@ public class ClimbableTrigger : MonoBehaviour
         //    }
         //}
         */
-
+ 
         if (!isOutsideBounds) return;
         playerSystem.IsClimbing = false;
     }
-
-
+ 
+ 
     #region OnTrigger Functions
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         playerSystem = other.GetComponent<PlayerSystem>();
     }
-
+ 
     private void OnTriggerExit(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         playerSystem = null;
-
+ 
         SkipJumpToClimbCheck = false;
     }
     #endregion
