9fd1fd9cbbe366e10a52805185aa8549b3b9209e
Josh <zurcasj.business@gmail.com>
2024-09-04 12:12:42 +1000

    [CHANGE] Made MovingHazardTrigger.cs (now LerpingObject.cs) more versatile.

---
+++ Assets/Scripts/Gameplay/ClutterClump.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ClutterClump : MonoBehaviour
+{
+    // - clump is several gameobjects stuck to each other.
+    // - is only intended to be floating in water, so it should be buoyant.
+
+    // - tell the clump to appear after all items in array have been marked as "knocked down".
+    // - if clump is touched by player, delay before clump crumbles apart.
+    // - after another delay, clump comes back
+
+
+    #region Public Variables
+
+    #endregion
+}
