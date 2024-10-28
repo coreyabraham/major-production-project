7f5bdd74d96159f102ccae12066e77de67b41684
Josh <zurcasj.business@gmail.com>
2024-10-28 14:24:31 +1100

    [CHANGE] Made some changes to my scene along with the scripts related to falling objects. Now it's possible to access the "isActive" bool without having to alter its protection level. On top of that, ActivateFalling script has been renamed and cleaned up.

---
+++ Assets/Scripts/Gameplay/ActivateFallingObjSpawner.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class ActivateFallingObjSpawner : MonoBehaviour
+{
+    #region Public Variables
+    [Header("References to Spawners")]
+
+    [field: Tooltip("References to all of the spawners that will be enabled when the player walks into this trigger.")]
+    [field: SerializeField] FallingObjectSpawner[] spawnerScript;
+
+    [Header("Bools")]
+
+    [field: Tooltip("Only allow the player to activate this trigger once. Subsequent attempts to activate this trigger will be ignored.")]
+    [field: SerializeField] bool onlyActivateOnce = true;
+    [field: Tooltip("Set all of the referenced spawners' \"Is Active\" variables to false when the scene starts.")]
+    [field: SerializeField] bool disableReferencesOnStart;
+    #endregion
+
+    #region Private Variables
+    bool hasActivatedOnce;
+    #endregion
+
+    #region Private Functions
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player") || spawnerScript.Length == 0 && (onlyActivateOnce && !hasActivatedOnce)) { return; }
+
+        if (spawnerScript.Length == 1)
+        {
+            spawnerScript[0].ToggleActiveState(true);
+        }
+        else
+        {
+            for (int i = 0; i < spawnerScript.Length; i++)
+            {
+                spawnerScript[i].ToggleActiveState(true);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        if (disableReferencesOnStart && spawnerScript.Length != 0)
+        {
+            if (spawnerScript.Length == 1)
+            {
+                spawnerScript[0].ToggleActiveState(false);
+            }
+            else
+            {
+                for (int i = 0; i < spawnerScript.Length; i++)
+                {
+                    spawnerScript[i].ToggleActiveState(false);
+                }
+            }
+        }
+    }
+    #endregion
+}
--- Assets/Scripts/Gameplay/FallingObjectSpawner.cs
+++ Assets/Scripts/Gameplay/FallingObjectSpawner.cs
@@ -21,6 +21,8 @@ public class FallingObjectSpawner : MonoBehaviour
     [field: SerializeField] float spawnrate;
     [field: Tooltip("The amount of time in seconds between instantiations.")]
     [field: SerializeField] float spawnDelay;
+    [field: Tooltip("The amount of prefabs that are allowed to be spawned before the spawner stops.\n\nSet to 0 for unlimited spawning.")]
+    [field: SerializeField] int spawnLimit;
 
     [field: Header("Rotation")]
 
@@ -36,10 +38,16 @@ public class FallingObjectSpawner : MonoBehaviour
     BoxCollider box;
     Vector3 posToSpawnAt;
     Quaternion angToSpawnAt;
+    [HideInInspector] public int amountSpawned = 0;
     #endregion
 
 
+    #region Public Functions
+    public void ToggleActiveState(bool setActive) => isActive = setActive;
+    #endregion
 
+
+    #region Private Functions
     private void InstantiateObject(int index)
     {
         GameObject objToSpawn = objects[index];
@@ -64,6 +72,7 @@ public class FallingObjectSpawner : MonoBehaviour
         //{ Debug.LogError("Prefab doesn't have a rigidbody component! It needs a rigidbody to work properly!"); isActive = false; return; }
 
         Instantiate(objToSpawn, posToSpawnAt, angToSpawnAt);
+        amountSpawned++;
 
         timer = 0;
     }
@@ -72,7 +81,7 @@ public class FallingObjectSpawner : MonoBehaviour
     private void Update()
     {
         timer += Time.deltaTime * spawnrate;
-        if (objects.Length <= 0 || !isActive) { return; }
+        if (objects.Length <= 0 || !isActive || (spawnLimit != 0 && amountSpawned >= spawnLimit)) { return; }
 
         if (timer >= spawnDelay)
         {
@@ -87,4 +96,5 @@ public class FallingObjectSpawner : MonoBehaviour
     {
         box = GetComponent<BoxCollider>();
     }
+    #endregion
 }
