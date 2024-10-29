784a90e687e9c469d9a192ecd56f69942c98ef3c
Josh <zurcasj.business@gmail.com>
2024-10-30 10:58:58 +1100

    [UPDATE] PipeFunctionality now has an isPermaSlippery bool. All it does currently is allow for external scripts to see if it's marked as such, since the actual slipping will need to be done in the PlayerSystem. [REMOVE] ClimbPoint script has been hurled into the stratosphere since it is going completely unused.

--- Assets/Scripts/Gameplay/PipeFunctionality.cs
+++ Assets/Scripts/Gameplay/PipeFunctionality.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+#region Enums - Public
 public enum PipeSide
 {
     Left = 0,
@@ -11,28 +12,40 @@ public enum PipeAxis
     X = 0,
     Z
 }
+#endregion
 
 public class PipeFunctionality : MonoBehaviour
 {
-    [field: Header("Pipe Specifics")]
+    #region Variables - Public
+    [field: Header("Pipe-Specific Variables")]
 
+    [field: Tooltip("If true, the pipe automatically makes the player slide down. They will not be able to control themselves and will be forced to slide on this pipe. The only way off is to either jump or get detached at the bottom of the pipe.\n\nIf this is true, the other two Pipe-Specific Variables will be automatically set.")]
+    [field: SerializeField] bool isPermaSlippery;
     [field: Tooltip("Does one end of this pipe connect to the ground?\nCan the player run into this pipe while grounded?")]
     [field: SerializeField] bool isConnectedToGround;
     [field: Tooltip("Does the player automatically detach from the pipe when they reach the bottom of it?")]
     [field: SerializeField] bool canDetachAtBottom;
+
+    [field: Header("Player-Specific Variables")]
+
     [field: Tooltip("The side of the pipe that the player will attach to when interacting with this pipe.")]
     [field: SerializeField] PipeSide sideToAttachTo;
     [field: Tooltip("In case the incorrect axis is being used when attaching to the pipes, adjust them using this.")]
     [field: SerializeField] PipeAxis axis;
+    #endregion
 
-    
+    #region Variables - Private
     private bool SkipJumpToClimbCheck = false;  // Prevents player from reattaching to current pipe if true.
     private float SkipJumpCooldown = 1;
     private PlayerSystem playSys;
 
     private BoxCollider Trig;
+    #endregion
 
-
+    #region Functions - Public
+    public bool GetSlipperyState() => isPermaSlippery;
+    #endregion
+    
     #region Functions - Private
     private void ReferencePlayer()
     {
@@ -130,6 +143,8 @@ public class PipeFunctionality : MonoBehaviour
     {
         Trig = GetComponent<BoxCollider>();
         if (isConnectedToGround) { canDetachAtBottom = true; }
+
+        if (isPermaSlippery) { isConnectedToGround = false; canDetachAtBottom = true; }
     }
     #endregion
 }
