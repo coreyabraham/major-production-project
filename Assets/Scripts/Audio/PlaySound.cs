015542700d3f4f001722edcb7fa6bc463222d545
Josh <zurcasj.business@gmail.com>
2024-11-20 11:47:15 +1100

    [ADD] Created PlaySound, which is a script that serves as a sort of hub for any audio that an object needs to use. [CHANGE] TimedHazardTrigger utilises PlaySound if they're on the same object. As of now, it's hard-coded for use with the stovetop flames. [CHANGE] The above changes have been set up and demonstrated in my scene.

---
+++ Assets/Scripts/Audio/PlaySound.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+// Used for playing audio on certain hazards.
+// Serves as a hub of sorts for all audio clips for a given object or hazard.
+// May not be compatible with everything. Ask programmers first.
+public class PlaySound : MonoBehaviour
+{
+    #region Public Variables
+    [field: SerializeField] AudioSource[] audioClips;
+
+    [field: Tooltip("Sets all referenced audio clips to disabled when the scene starts. This ensures they won't accidentally be left on.")]
+    [field: SerializeField] bool disableOnStart = true;
+    #endregion
+
+
+    #region Public Functions
+    public void PlaySoundOnce(int index) => audioClips[index].enabled = true;
+    public void StopSoundOnce(int index) => audioClips[index].enabled = false;
+    #endregion
+
+
+    #region Private Functions
+    private void Start()
+    {
+        for (int i = 0; i < audioClips.Length; i++) { audioClips[i].enabled = false; }
+    }
+    #endregion
+}
