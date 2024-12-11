589207de33649caef77280426593b6646b3cace0
Josh <zurcasj.business@gmail.com>
2024-12-11 17:48:16 +1100

    [CHANGE] Some more changes for the showcase build. As of now (and because I can't fix them), the following problems still exist: - The second crow in the alleyway scene does NOT fly towards the player when they enter the hazard trigger. - When the player is flung from the mop in the kitchen, no UI overlays the screen nor does an end screen appear.

--- Assets/Scripts/Gameplay/CrowMovement.cs
+++ Assets/Scripts/Gameplay/CrowMovement.cs
@@ -9,6 +9,11 @@ public class CrowMovement : MonoBehaviour
 
     // Check for playing Death Animation
     public bool attackPlayer;
+
+    [field: Tooltip("Reference to the player model's Animator that's used to animate the character.")]
+    public Animator animator;
+    [field: Header("Collections")]
+    [field: SerializeField] PlayerAnimation[] crowAnimations;
     #endregion
 
 
@@ -22,8 +27,9 @@ public class CrowMovement : MonoBehaviour
     private CrowAwareness awareness;
 
     // Reset Position on Death
-    private float resetTimer = 0.0f;
+    private float resetTimer = 0.0f, landingResetTimer = 0.0f;
     private bool resetTriggered = false;
+    private bool IsLanding, IsFlying;
     #endregion
 
 
@@ -34,11 +40,23 @@ public class CrowMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (IsLanding)
+        {
+            landingResetTimer += Time.fixedDeltaTime;
+            if (landingResetTimer >= 1)
+            {
+                IsLanding = false;
+                IsFlying = false;
+                landingResetTimer = 0.0f;
+            }
+        }
+
         if (!rb || !awareness) { return; }
 
         if (awareness.GetAwareness())
         {
             resetTriggered = true;
+            IsFlying = true;
 
             // Rotate crow to face target.
             targetDirection = awareness.GetDirection();
@@ -50,6 +68,8 @@ public class CrowMovement : MonoBehaviour
         if (!resetTriggered) { return; }
 
         resetTimer += Time.fixedDeltaTime;
+
+        if (resetTimer >= 0.8f) { IsFlying = false; IsLanding = true; }
 
         if (resetTimer > 1f)
         {
@@ -66,6 +86,29 @@ public class CrowMovement : MonoBehaviour
             resetTriggered = false;
 
             awareness.ResetAwareness();
+        }
+    }
+
+    private void Update()
+    {
+        foreach (PlayerAnimation anim in crowAnimations)
+        {
+            if (anim.InternalAnimType != AnimType.Custom)
+            {
+                switch (anim.InternalAnimType)
+                {
+                    case AnimType.IsFlying: animator.SetBool(anim.Name, IsFlying); break;
+                    case AnimType.IsLanding: animator.SetBool(anim.Name, IsLanding); break;
+                }
+                continue;
+            }
+            switch (anim.CustomValueType)
+            {
+                case AnimValueType.Boolean:
+                    if (!bool.TryParse(anim.CustomValue, out bool resultBool)) return;
+                    animator.SetBool(anim.Name, resultBool);
+                    break;
+            }
         }
     }
 
